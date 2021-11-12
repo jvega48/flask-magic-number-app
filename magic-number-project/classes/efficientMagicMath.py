f379b594464922b0c0b0d97cea4abb9aded7f3e6
jvega48 <jvega4849@gmail.com>
2021-11-11 19:13:10 -0800

    This api will transform the response to a list of dog breeds including the sub breeds as a separate element under the main breed creatinga single list of strings.

---
+++ magic-number-project/classes/efficientMagicMath.py
@@ -0,0 +1,23 @@
+class EfficientMagicMath:
+    def __init__(self):
+        pass
+
+    def magicNumber(self, n, dictionary):
+        result = 0
+        
+        if dictionary is None:
+            dictionary = {}
+
+        if n in dictionary:
+            return dictionary[n]
+        
+        if n == 0:
+            return 0
+        
+        if n == 1:
+            return 1
+        
+        result = n + self.magicNumber(n-1, dictionary) + self.magicNumber(n-2, dictionary)
+        dictionary[n] = result
+        
+        return result
